f0d4f8ea590b2a055c602fb014d1012957959524
dream_runner <42798210+dreamrunner1996@users.noreply.github.com>
2020-06-06 16:55:31 +0800

    v0.0.1

---
+++ src/components/IntegrateCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="integrate-card-content">
+    <div class="integrate-card-image">
+      <img v-if="imgUrl" :src="imgUrl" alt="#">
+      <img v-else src="./../assets/userInfo.png" alt="#" />
+    </div>
+    <div class="integrate-card-information">
+      <div class="integrate-card-information-text">
+        {{ name }}
+      </div>
+      <div class="integrate-card-information-integration">
+        当前积分: {{ integration }}
+      </div>
+    </div>
+    <div class="integrate-card-award">
+      <img v-if="showJB" :src="jbImgUrl[rank - 1]" alt="#" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import "./integrate-card.css"
+
+  export default {
+    name: "IntegrateCard",
+    props: {
+      imgUrl: {
+        default: "",
+        type: String
+      },
+      name: {
+        default: "张三丰",
+        type: String
+      },
+      integration: {
+        default: 0,
+        type: Number
+      },
+      rank: {
+        default: 0,
+        type: Number
+      }
+    },
+    computed: {
+      showJB: function() {
+        switch (this.rank) {
+          case 1:
+            return true
+          case 2:
+            return true
+          case 3:
+            return true
+          default:
+            return false
+        }
+      }
+    },
+    data: () => ({
+      jbImgUrl: [
+        require("./../assets/jb1.png"),
+        require("./../assets/jb2.png"),
+        require("./../assets/jb3.png")
+      ]
+    })
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
       <div class="notify-card">
         <img src="./../assets/notify.png" alt="#">
         <div class="notify-content">
+          <span v-if="!notifyMessage">{{ dashboardName }}</span>
           <Notify :notifyMessage="notifyMessage" />
         </div>
       </div>
@@ -15,9 +16,32 @@
       </div>
     </div>
     <div class="body-card">
-      <div class="body-card-left"></div>
-      <div class="body-card-center"></div>
-      <div class="body-card-right"></div>
+      <div class="body-card-left">
+        <div class="body-card-left-header">
+          <div class="body-card-left-header-text">
+            昨日积分排名
+          </div>
+          <div class="body-card-left-header-img">
+            <img src="./../assets/jx.png" alt="#">
+          </div>
+        </div>
+        <div class="body-card-left-body">
+          <div class="body-card-left-body-scroll" :style="integrateCardList.id.length > 6 ? styleList.bodyCardLeftBodyScroll : styleList.bodyCardLeftBodyUnScroll">
+            <IntegrateCard v-for="(index, item) in integrateCardList.id"
+                           :key="'integration' + index"
+                           :imgUrl="integrateCardList.imgUrl[item]"
+                           :name="integrateCardList.name[item]"
+                           :integration="integrateCardList.integration[item]"
+                           :rank="integrateCardList.rank[item]" />
+          </div>
+        </div>
+      </div>
+      <div class="body-card-center">
+        <button @click="test">test</button>
+      </div>
+      <div class="body-card-right">
+
+      </div>
     </div>
   </div>
 </template>
@@ -25,27 +49,99 @@
 <script>
   import Time from "./../components/Time"
   import Notify from "../components/Notify"
+  import IntegrateCard from "../components/IntegrateCard"
 
   import "./home.css"
 
+  const bodyCardLeftBodyUnScroll = {
+
+  }
+
+  const bodyCardLeftBodyScroll = {
+    marginTop: 0,
+    animation: "bodyCardLeftBodyScroll 10s linear infinite"
+  }
+
   export default {
     name: "Home",
-    components: { Time, Notify },
+    components: { Time, Notify, IntegrateCard },
     data: () => ({
-      fleshWeather: true,
-      weather: {},
-      notifyMessage: "这是一条通知信息"
+      // 全局出现的 style 动态样式
+      styleList: {
+        bodyCardLeftBodyUnScroll,
+        bodyCardLeftBodyScroll
+      },
+      // 没有通知时显示标题名
+      dashboardName: "利事丰员工积分看板",
+      // 通知信息
+      notifyMessage: "",
+      // 左边积分排行信息列表
+      integrateCardList: {
+        id: [0, 1, 2, 3, 4, 5],
+        imgUrl: [
+          require("./../assets/wx1.jpg"),
+          require("./../assets/robot.png"),
+          require("./../assets/robot.png"),
+          require("./../assets/robot.png"),
+          require("./../assets/robot.png"),
+          require("./../assets/robot.png")
+        ],
+        name: [
+          "robot1", "robot2", "robot3", "robot4", "robot5", "robot6"
+        ],
+        integration: [
+          99, 97, 95, 92, 88, 82
+        ],
+        rank: [1, 2, 3, 4, 5, 6]
+      },
+      // 中间积分列表
+      integrateFirstList: {
+        id: 0,
+        imgUrl: require("./../assets/wx1.jpg"),
+        name: "robot1",
+        integration: 99
+      },
+      // 右边异常列表
+      unusualList: {}
     }),
     mounted() {
+      let _this = this
+      // 更新排名第一 --- 中间部分
+      _this.FirstUpdate()
     },
     methods: {
-      HourChange() {
+      // 测试
+      test() {
         let _this = this
-        _this.fleshWeather = false
-        _this.$nextTick( () => {
-          _this.fleshWeather = true
-        } )
-      }
+        _this.integrateCardList.id.push(_this.integrateCardList.id.length+1)
+        _this.integrateCardList.imgUrl.push("");
+        _this.integrateCardList.name.push("robot?");
+        _this.integrateCardList.integration.push(77);
+        _this.integrateCardList.rank.push(8);
+        console.log(120 * (_this.integrateCardList.id.length - 6) - 160)
+        if (_this.integrateCardList.id.length > 6) {
+          document.styleSheets[0].deleteRule(0)
+          document.styleSheets[0].insertRule(`@keyframes bodyCardLeftBodyScroll
+            {
+              0% {
+                margin-top: 0;
+              }
+              25% {
+                  margin-top: calc(${-20 - 120 * (_this.integrateCardList.id.length - 7)}px);
+              }
+              50% {
+                  margin-top: calc(${-20 - 120 * (_this.integrateCardList.id.length - 7)}px);
+              }
+              75% {
+                  margin-top: 0;
+              }
+            }`,0)
+        }
+
+      },
+      HourChange() {},
+      // 排行榜第一更新
+      FirstUpdate() {}
     }
   }
 </script>
