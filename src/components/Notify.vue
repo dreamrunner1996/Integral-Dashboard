c115b4cb05bc76e2b05c8d7543c6681c2043339f
dream_runner <42798210+dreamrunner1996@users.noreply.github.com>
2020-06-06 13:47:58 +0800

    v0.0.1

---
+++ src/components/Notify.vue
@@ -0,0 +1,27 @@
+<template>
+  <marquee class="notify-content" direction="left">
+    {{ notifyMessage }}
+  </marquee>
+</template>
+
+<script>
+  import "./notify.css"
+
+  export default {
+    name: "Notify",
+    props: {
+      notifyMessage: {
+        default: "",
+        type: String
+      }
+    },
+    data: () => ({}),
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="home-content">
-    <div class="home-left-page">
-
-    </div>
-    <div class="home-right-page">
-      <div class="top-card">
+    <div class="top-card">
+      <div class="logo">
         <img src="./../assets/robot.png" alt="#">
+      </div>
+      <div class="notify-card">
+        <img src="./../assets/notify.png" alt="#">
+        <div class="notify-content">
+          <Notify :notifyMessage="notifyMessage" />
+        </div>
+      </div>
+      <div class="time-card">
         <Time @hourChange="HourChange" />
       </div>
-      <Weather v-if="fleshWeather" />
+    </div>
+    <div class="body-card">
+      <div class="body-card-left"></div>
+      <div class="body-card-center"></div>
+      <div class="body-card-right"></div>
     </div>
   </div>
 </template>
 
 <script>
   import Time from "./../components/Time"
-  import Weather from "./../components/Weather"
+  import Notify from "../components/Notify"
 
-  import "./home.css";
+  import "./home.css"
 
   export default {
     name: "Home",
-    components: { Time, Weather },
+    components: { Time, Notify },
     data: () => ({
       fleshWeather: true,
-      weather: {}
+      weather: {},
+      notifyMessage: "这是一条通知信息"
     }),
     mounted() {
     },
