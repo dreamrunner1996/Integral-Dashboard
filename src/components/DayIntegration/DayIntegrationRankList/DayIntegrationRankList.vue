2204b4fa6d0b1da86d4d0ed690081094f5dd1d44
dream_runner <42798210+dreamrunner1996@users.noreply.github.com>
2020-06-27 16:30:54 +0800

    reset - v0.0.1

---
+++ src/components/DayIntegration/DayIntegrationRankList/DayIntegrationRankList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="day-integration-rank-list-content">
+    <div class="day-integration-rank-list-item">
+      <div class="day-integration-rank-list-content-image">
+        <img :src="logo" alt="#">
+      </div>
+      <div class="day-integration-rank-list-content-name-pm">
+        <div class="name"> {{ name }} </div>
+        <div class="rank"> NO.{{ rank }} </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import "./DayIntegrationRankList.css"
+
+  export default {
+    name: "DayIntegrationRankList",
+    props: {
+      name: {
+        default: "名字",
+        type: String
+      },
+      logo: {
+        default: "",
+        type: String
+      },
+      rank: {
+        default: 0,
+        type: Number
+      },
+    },
+    data: () => ({})
+  }
+</script>
---
+++ src/components/MonthIntegration/MonthIntegrationRankListBack/MonthIntegrationRankListBack.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="month-integration-rank-list-back-content">
+    <div class="user-info-logo">
+      <img :src="logo" alt="#">
+    </div>
+    <div class="user-info-name-rank">
+      <div class="user-info-name">{{ name }}</div>
+      <div class="user-info-rank">NO.{{ rank }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import "./MonthIntegrationRankListBack.css"
+
+  export default {
+    name: "MonthIntegrationRankListBack",
+    props: {
+      logo: {
+        default: '',
+        type: String
+      },
+      name: {
+        default: '',
+        type: String
+      },
+      rank: {
+        default: 0,
+        type: Number
+      }
+    },
+    data: () => ({})
+  }
+</script>
