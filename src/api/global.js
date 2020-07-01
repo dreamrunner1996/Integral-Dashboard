829f5071a9e97de3bb535705633aa926145d06d6
dream_runner <42798210+dreamrunner1996@users.noreply.github.com>
2020-07-01 08:22:51 +0800

    update - 2020-06-29 - 17:00

--- src/api/global.js
+++ src/api/global.js
@@ -1,18 +1,18 @@
-let dev = {
-  url: "http://192.168.3.82",
-  port: "8000"
-}
+// let dev = {
+//   url: "http://192.168.3.82",
+//   port: "8000"
+// }
 // let dev2 = {
 //   url: "http://192.168.3.245",
 //   port: "8000"
 // }
-// let server = {
-//   url: "http://api.gdusaful.cn",
-//   port: "81"
-// }
+let server = {
+  url: "http://api.gdusaful.cn",
+  port: "81"
+}
 
 const api = {
-  ...dev,
+  ...server,
   path: "/api/teamDashboard/show",
   file: "/avatar/",
   condition: {
